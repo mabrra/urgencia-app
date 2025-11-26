495c5a662da8eeaca0cd08fc7140e2b64f929433
Martín Abarca <martin@MacBook-Air-de-Martin.local>
2025-11-26 00:04:10 -0300

    Corrigiendo credenciales de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,10 @@ import { getFirestore } from "firebase/firestore";
 // --- PEGA AQUÍ TUS DATOS DE FIREBASE ---
 const firebaseConfig = {
   apiKey: "AIzaSyDhyHXyBxIh_k3HqDVaDn7DsPXjUs4-nwc",
-
   authDomain: "urgencia-martin.firebaseapp.com",
-
   projectId: "urgencia-martin",
-
   storageBucket: "urgencia-martin.firebasestorage.app",
-
   messagingSenderId: "608099470654",
-
   appId: "1:608099470654:web:d2dd3f552e4464fa7cb06f"
 };
 // ----------------------------------------
